28c2106acca6bb6bb7074e6fc4397bd821f3fe9f
EminPetlaca <eminpetlacanpp@gmail.com>
2024-05-02 17:10:43 +0200

    drop down start

---
+++ src/components/Menus/DropDowns/UredniDrop.jsx
@@ -0,0 +1,15 @@
+import "./UredniDrop.css";
+
+
+
+export default function UredniDrop() {
+  return (
+    <>
+        {/* drop down of link "Uredni" */}
+    <div className="drop-container">
+    <p>menu</p>
+    </div>
+
+    </>
+  )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Header from '../../components/Header/Header';
+import UredniDrop from '../../components/Menus/DropDowns/UredniDrop';
 
 
 export default function Home() {
   return (
     <>
       <Header />
+     
       <h1>Home</h1>
     </>
   );
