b883d45babf820b8adb8d705883ed72e4eb6ec9c
Kapp1cka <simon.soltes07@gmail.com>
2024-05-07 22:31:47 +0200

    stracim to

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -52,7 +52,7 @@ export default function Header() {
 
       <hr />
 
-      {isObecDropVisible && <ObecDrop />}
+      {isObecDropVisible && <ObecDrop className="header-obecDrop" />}
     </>
   );
 }
--- src/components/Menus/DropDowns/ObecDrop.jsx
+++ src/components/Menus/DropDowns/ObecDrop.jsx
@@ -8,60 +8,60 @@ export default function ObecDrop() {
       <div className="drop-container">
         <ul className="drop-menu">
           <li>
-            <Link to="/verejne-vyhlasky">Veřejné vyhlášky</Link>
+            <Link classname="link" to="/verejne-vyhlasky">Veřejné vyhlášky</Link>
           </li>
           <li>
-            <Link to="/pozvanky">Pozvánky na jednání zastupitelstva</Link>
+            <Link classname="link" to="/pozvanky">Pozvánky na jednání zastupitelstva</Link>
           </li>
           <li>
-            <Link to="/zapisy">Zasedání zastupitelstva obce</Link>
+            <Link classname="link" to="/zapisy">Zasedání zastupitelstva obce</Link>
           </li>
           <li>
-            <Link to="/zpravodaj">Čimelický zpravodaj</Link>
-          </li>
-        </ul>
-        <ul className="drop-menu">
-          <li>
-            <Link to="/vystava-kvetin">Výstava květin Čimelice</Link>
-          </li>
-          <li>
-            <Link to="/pronajem-kurtu">Pronájem kurtů a tělocvičny</Link>
-          </li>
-          <li>
-            <Link to="/seznam-podnikatelu">Seznam podnikatelů a služeb</Link>
-          </li>
-          <li>
-            <Link to="/jizdni-rady">Jízdní řady</Link>
+            <Link classname="link" to="/zpravodaj">Čimelický zpravodaj</Link>
           </li>
         </ul>
         <ul className="drop-menu">
           <li>
-            <Link to="/sbor-dobrovolnych-hasicu">
+            <Link classname="link" to="/vystava-kvetin">Výstava květin Čimelice</Link>
+          </li>
+          <li>
+            <Link classname="link" to="/pronajem-kurtu">Pronájem kurtů a tělocvičny</Link>
+          </li>
+          <li>
+            <Link classname="link" to="/seznam-podnikatelu">Seznam podnikatelů a služeb</Link>
+          </li>
+          <li>
+            <Link classname="link" to="/jizdni-rady">Jízdní řady</Link>
+          </li>
+        </ul>
+        <ul className="drop-menu">
+          <li>
+            <Link classname="link" to="/sbor-dobrovolnych-hasicu">
               Dobrovolný hasiči Čimelice
             </Link>
           </li>
           <li>
-            <Link to="/zajmova-sdruzeni">Zajmová sdružení pro volný čas</Link>
+            <Link classname="link" to="/zajmova-sdruzeni">Zajmová sdružení pro volný čas</Link>
           </li>
           <li>
-            <Link to="/turistika-sluzby">Turistika, zajímavosti a služby</Link>
+            <Link classname="link" to="/turistika-sluzby">Turistika, zajímavosti a služby</Link>
           </li>
           <li>
-            <Link to="/sport">Sport</Link>
+            <Link classname="link" to="/sport">Sport</Link>
           </li>
         </ul>
         <ul className="drop-menu">
           <li>
-            <Link to="/dobrovolny-svazek">Dobrovolný svazek Písecka</Link>
+            <Link classname="link" to="/dobrovolny-svazek">Dobrovolný svazek Písecka</Link>
           </li>
           <li>
-            <Link to="/dokumenty-sorp">Dokumenty SORP</Link>
+            <Link classname="link" to="/dokumenty-sorp">Dokumenty SORP</Link>
           </li>
           <li>
-            <Link to="/mapovy-portal">Náš mapový portál</Link>
+            <Link classname="link" to="/mapovy-portal">Náš mapový portál</Link>
           </li>
           <li>
-            <Link to="/verejna-sprava">Portál veřejné správy</Link>
+            <Link classname="link" to="/verejna-sprava">Portál veřejné správy</Link>
           </li>
         </ul>
       </div>
