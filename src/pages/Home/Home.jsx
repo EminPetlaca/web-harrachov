e82ee649a9dec446c750e4718f3c9c1ffe67991e
Kapp1cka <simon.soltes07@gmail.com>
2024-05-15 19:15:53 +0200

    futr

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import ObecDrop from '../../components/Menus/DropDowns/ObecDrop';
 import MainSection from '../../components/Sections/MainSection/MainSection';
 import NewsSection from '../../components/Sections/NewsSection/NewsSection';
 import ZnakSection from '../../components/Sections/ZnakSection/ZnakSection';
-
+import Footer from '../../components/Footer/Footer';
 
 export default function Home() {
   return (
@@ -12,7 +12,7 @@ export default function Home() {
       <MainSection/>
       <NewsSection />
       <ZnakSection/>
-      
+      <Footer />
      
     </>
   );
